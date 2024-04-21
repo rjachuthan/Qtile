9864dda35fc940c4619c5f5e877b89f60eb13255
rjachuthan <riturajachuthan@gmail.com>
2024-04-21 20:57:33 +0530

    Enable Gnome Polkit

--- autostart.sh
+++ autostart.sh
@@ -5,13 +5,14 @@ syncthing --no-browser &
 syncthingtray &
 qtile run-cmd -g 1 thorium-browser &
 qtile run-cmd -g 2 logseq &
-qtile run-cmd -g 3 qutebrowser www.youtube.com &
+qtile run-cmd -g 8 anki &
+# qtile run-cmd -g 3 qutebrowser www.youtube.com &
 picom &
 
 # starting utility applications at boot time
 # picom --config $HOME/.config/picom/picom.conf --vsync &
-# /usr/libexec/polkit-gnome-autentication-agent-1 &
-# /usr/lib/xfce4/notifyd/xfce4-notifyd &
-# playerctld daemon &
-# xhost +si:localuser:$USER &
+/usr/libexec/polkit-gnome-autentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+playerctld daemon &
+xhost +si:localuser:$USER &
 # ~/.fehbg &
